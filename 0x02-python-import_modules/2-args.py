a93a7ecc9520861f253db4a68398417c587e2574
sharon <dolivon555@gmail.com>
2022-09-22 12:43:17 +0000

    push

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if i <= 1:
         print("0 arguments.")
     elif i == 2:
-        print("{} arguments:".format(i - 1))
+        print("{} argument:".format(i - 1))
     else:
         print("{} arguments:".format(i - 1))
-        for i in range(len(sys.argv) - 1):
-            print("{:d}: {}".format(i + 1, sys.argv[i + 1]))
+    for i in range(len(sys.argv) - 1):
+        print("{:d}: {}".format(i + 1, sys.argv[i + 1]))
