025d33b8e4c90354a5916d78bebfdc2257a4131c
sharon <dolivon555@gmail.com>
2022-10-11 16:39:30 +0000

    push

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for private instant attribute size"""
+
+
+class Square:
+    """Define a square 
+    """
+    def __init__(self, size):
+        """Initialize the size
+    
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
