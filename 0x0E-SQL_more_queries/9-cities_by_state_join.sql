4ba7dd72a88c6ae6eef659f6f4cec9a30d79abd1
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-12-01 19:36:38 +0100

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
+-- Prints and joins records from two tables with a matching field
 SELECT cities.id, cities.name, states.name
     FROM cities
     INNER JOIN states
-        ON cities.state_id = state_id
+        ON cities.state_id = states.id
     ORDER BY cities.id ASC;
