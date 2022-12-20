3412bd19f1c89d93ae73181d72aa8abc79b6affc
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-12-20 10:53:59 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const rarg = process.argv.length - 2;
+const rargv = process.argv.length - 2;
 
-if (rarg == 0) {
-console.log('No argument');
-} else if (rarg == 1) {
-console.log('Argument found');
+if (rargv === 0) {
+  console.log('No argument');
+} else if (rargv === 1) {
+  console.log('Argument found');
 } else {
-console.log('Arguments found');
+  console.log('Arguments found');
 }
