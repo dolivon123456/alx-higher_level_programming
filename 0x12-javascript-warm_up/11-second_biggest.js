8a3bfb428f94fc25bd54b56a5841ba2c925cee30
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-12-20 11:56:21 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-let args = process.argv.slice(2).map((x) => {
-  return parseInt(x);
-});
 
-if (args.length <= 1) {
+const size = process.argv.length;
+
+if (size <= 3) {
   console.log(0);
 } else {
-  console.log(args.sort((a, b) => {
-    return b - a;
-  })[1]);
+  const newA = process.argv.map(Number);
+  newA.slice(2, size);
+  newA.sort((a, b) => a - b);
+  console.log(newA[newA.length - 2]);
 }
