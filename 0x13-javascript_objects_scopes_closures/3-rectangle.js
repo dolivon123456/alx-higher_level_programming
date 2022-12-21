f0bcb15c37db5f635917e3b2d4bd454803a820d1
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-12-21 09:00:09 +0100

    Create 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  print () {
+    const Rect = 'X'.repeat(this.width);
+    for (i = 0; i < this.height; i++) {
+      console.log(Rect);
+    }
+  }
+}
+module.exports = Rectangle;
