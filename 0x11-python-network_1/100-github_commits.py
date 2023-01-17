f12c55be969cf9f85b20d0ba014016fe20bf11f5
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2023-01-17 22:38:42 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-
+"""lists the 10 most recent commits on a given GitHub repository.
 """
-Lists 10 commits from th erepo 'rails'
-"""
-
 import sys
 import requests
-if __name__ == '__main__':
-    url = 'https://api.github.com/repos/'
-    r = requests.get(url + '{}/{}/commits?per_page=10'.format(
-                                    sys.argv[1],
-                                    sys.argv[2]))
-    js = r.json()
-    for i in range(10):
-        print("{}: {}".format(js[i].get('sha'),
-                              js[i].get('commit').get('author').get('name')))
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
